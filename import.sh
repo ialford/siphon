c3a2bd2bf4520624a290dc68e8137357233db8c4
Bill Sill <wsill@nd.edu>
2021-09-02 14:19:01 -0400

    use bundle exec to run import

--- import.sh
+++ import.sh
@@ -2,4 +2,5 @@
 set -e
 
 echo "Run the Aleph Importer"
-exec bash -lc 'rails runner AlephReformattingImporter.new.import!'
+# exec bash -lc 'rails runner AlephReformattingImporter.new.import!'
+exec bundle exec rails runner AlephReformattingImporter.new.import!
